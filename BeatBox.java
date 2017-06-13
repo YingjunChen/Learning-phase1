d22b7ba02c5c9806052439a07601e0d9e0ebd340
ChenZhaoyi96 <425547874@qq.com>
2017-06-13 23:09:45 +0800

    BeatBox添加输入输出功能

--- BeatBox.java
+++ BeatBox.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import javax.sound.midi.*;
 import java.util.*;
 import java.awt.event.*;
+import java.io.*;
 
 public class BeatBox{
 	
@@ -47,6 +48,14 @@ public class BeatBox{
 		JButton downTempo=new JButton("Tempo Down");
 		downTempo.addActionListener(new MyDownTempoListener());
 		buttonBox.add(downTempo);
+		
+		JButton serializelt=new JButton("serializelt");
+		serializelt.addActionListener(new MySendListener());
+		buttonBox.add(serializelt);
+		
+		JButton restore=new JButton("restore");
+		restore.addActionListener(new MyReadInListener());
+		buttonBox.add(restore);
 		
 		Box nameBox=new Box(BoxLayout.Y_AXIS);
 		for(int i=0;i<16;i++){
@@ -148,6 +157,48 @@ public class BeatBox{
 			sequencer.setTempoFactor((float)(tempoFactor*0.97));
 		}
 	}
+	public class MySendListener implements ActionListener{
+		public void actionPerformed(ActionEvent a){
+			boolean[] checkboxState=new boolean[256];
+			
+			for(int i=0;i<256;i++){
+				JCheckBox check=(JCheckBox) checkboxList.get(i);
+				if(check.isSelected()){
+					checkboxState[i]=true;
+				}
+			}
+			
+			try{
+				FileOutputStream fileStream=new FileOutputStream(new File("Checkbox.ser"));
+				ObjectOutputStream os=new ObjectOutputStream(fileStream);
+				os.writeObject(checkboxState);
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public class MyReadInListener implements ActionListener{
+		public void actionPerformed(ActionEvent a){
+			boolean[] checkboxState=null;
+			try{
+				FileInputStream fileIn=new FileInputStream(new File("Checkbox.ser"));
+				ObjectInputStream is=new ObjectInputStream(fileIn);
+				checkboxState=(boolean[]) is.readObject();
+			}catch(Exception ex){ex.printStackTrace();}
+			
+			for(int i=0;i<256;i++){
+				JCheckBox check=(JCheckBox) checkboxList.get(i);
+				if(checkboxState[i]){
+					check.setSelected(true);
+				}else{check.setSelected(false);
+				}
+			}
+			sequencer.stop();
+			buildTrackAndStart();
+			}
+	}
+	
 	
 	public void makeTracks(int[] list){
 		for(int i=0;i<16;i++){
